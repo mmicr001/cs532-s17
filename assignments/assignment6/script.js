5d6c07e6514d201330d3936b8a275d3159b21fcb
mmicr001 <mmicr001@odu.edu>
2017-03-20 21:19:07 -0400

    Add files via upload

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -1,7 +1,9 @@
-var w=960,h=500;
-var svg=d3.select("#chart")
-.append("svg")
-.attr("width",w).attr("height",h);
-svg
-.append("text")
-.text("hello world!").attr("x",200).attr("y",200);
+var w=960,h=500,flag=0;
+var svg=d3.select("#chart").append("svg").attr("width",w).attr("height",h);
+var myRect=svg
+.append( "rect").attr({x:100,y:100,width:100,height:100})
+.style("fill","steelblue");
+myRect.on("click",function() {
+flag=1-flag;
+myRect.style("fill", flag?"darkorange":"steelblue");
+})
