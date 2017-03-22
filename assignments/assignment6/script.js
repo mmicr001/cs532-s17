805d7df5ae09eb2c105029a2f819146a43a4ed50
mmicr001 <mmicr001@odu.edu>
2017-03-22 12:35:11 -0400

    Update script.js

--- assignments/assignment6/script.js
+++ assignments/assignment6/script.js
@@ -1,9 +1,1 @@
-var w=960,h=500,flag=0;
-var svg=d3.select("#chart").append("svg").attr("width",w).attr("height",h);
-var myRect=svg
-.append( "rect").attr({x:100,y:100,width:100,height:100})
-.style("fill","steelblue");
-myRect.on("click",function() {
-flag=1-flag;
-myRect.style("fill", flag?"darkorange":"steelblue");
-})
+
